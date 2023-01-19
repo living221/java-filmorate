ab10b6b7ed62e968df1158ad39abdb1d3a45b852
living221 <living221@gmail.com>
2023-01-19 16:58:30 +0700

    refactor: добавлены аннотации для тестирования. style: codestyle для тестовых методов.

--- src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/FilmControllerTest.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 
@@ -10,9 +12,11 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SpringBootTest
 class FilmControllerTest {
 
     private Film film;
+    @Autowired
     private FilmController controller;
 
     @BeforeEach
@@ -23,14 +27,13 @@ class FilmControllerTest {
         film.setDescription("film description");
         film.setReleaseDate(LocalDate.of(2000, 1, 1));
         film.setDuration(100);
-
-        controller = new FilmController();
     }
 
     @Test
     void validateFilmWithName() {
         film.setName(null);
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.createFilm(film));
+
         assertEquals("Film name cannot be empty!", ex.getMessage());
     }
 
@@ -38,6 +41,7 @@ class FilmControllerTest {
     void validateFilmBlankName() {
         film.setName("                    ");
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.createFilm(film));
+
         assertEquals("Film name cannot be empty!", ex.getMessage());
     }
 
@@ -48,6 +52,7 @@ class FilmControllerTest {
                 "sem nec commodo. Nam eu sollicitudin eros. Cras nec convallis erat. Nam at venenatis sem. " +
                 "Nullam augue ante efficitur. ");
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.createFilm(film));
+
         assertEquals("Film description length cannot be more than 200 signs.", ex.getMessage());
     }
 
@@ -56,10 +61,12 @@ class FilmControllerTest {
     void validateFilmIncorrectDate() {
         film.setReleaseDate(LocalDate.of(1895, 12, 28));
         controller.films.put(film.getId(), film);
+
         assertEquals(controller.films.values().size(), 1);
 
         film.setReleaseDate(LocalDate.of(1895, 12, 27));
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.createFilm(film));
+
         assertEquals("Film release date cannot be before 28.12.1895", ex.getMessage());
     }
 
@@ -68,25 +75,30 @@ class FilmControllerTest {
     void validateFilmDuration() {
         film.setDuration(1);
         controller.films.put(film.getId(), film);
+
         assertEquals(controller.films.values().size(), 1);
 
         film.setDuration(0);
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.createFilm(film));
+
         assertEquals("Film duration cannot be negative or zero.", ex.getMessage());
 
         film.setDuration(-1);
         ex = assertThrows(ValidationException.class, () -> controller.createFilm(film));
+
         assertEquals("Film duration cannot be negative or zero.", ex.getMessage());
     }
 
     @Test
     void updateFilm() {
         controller.createFilm(film);
+
         assertEquals(controller.films.values().size(), 1);
         assertEquals(film.getId(), 1);
 
         film.setId(Integer.MIN_VALUE);
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.updateFilm(film));
+
         assertEquals(String.format("Film with id: %s was not found!", film.getId()), ex.getMessage());
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/controller/UserControllerTest.java
@@ -2,6 +2,8 @@ package ru.yandex.practicum.filmorate.controller;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import ru.yandex.practicum.filmorate.exceptions.ValidationException;
 import ru.yandex.practicum.filmorate.model.User;
 
@@ -10,9 +12,11 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@SpringBootTest
 class UserControllerTest {
 
     private User user;
+    @Autowired
     private UserController controller;
 
     @BeforeEach
@@ -23,26 +27,28 @@ class UserControllerTest {
         user.setLogin("login");
         user.setEmail("example@email.com");
         user.setBirthday(LocalDate.of(1913, 5, 17));
-
-        controller = new UserController();
     }
 
     @Test
     public void validateUserEmail() {
         user.setEmail("            ");
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User email cannot be empty.", ex.getMessage());
 
         user.setEmail("");
         ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User email cannot be empty.", ex.getMessage());
 
         user.setEmail(null);
         ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User email cannot be empty.", ex.getMessage());
 
         user.setEmail("abcdefghijklmnop");
         ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User email have to contain '@' sign.", ex.getMessage());
     }
 
@@ -50,18 +56,22 @@ class UserControllerTest {
     public void validateUserLogin() {
         user.setLogin("            ");
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User login cannot be empty or blank.", ex.getMessage());
 
         user.setLogin("");
         ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User login cannot be empty or blank.", ex.getMessage());
 
         user.setLogin(null);
         ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User login cannot be empty or blank.", ex.getMessage());
 
         user.setLogin("login with spaces");
         ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User login cannot have spaces in it.", ex.getMessage());
     }
 
@@ -69,6 +79,7 @@ class UserControllerTest {
     public void validateUserNameBlank() {
         user.setName("            ");
         controller.createUser(user);
+
         assertEquals(user.getLogin(), user.getName());
     }
 
@@ -76,6 +87,7 @@ class UserControllerTest {
     public void validateUserNameNull() {
         user.setName(null);
         controller.createUser(user);
+
         assertEquals(user.getLogin(), user.getName());
     }
 
@@ -83,21 +95,25 @@ class UserControllerTest {
     public void validateUserBirthday() {
         user.setBirthday(LocalDate.MAX);
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.createUser(user));
+
         assertEquals("User birthday cannot be in the future", ex.getMessage());
 
         user.setBirthday(LocalDate.now());
         controller.createUser(user);
+
         assertEquals(controller.users.values().size(), 1);
     }
 
     @Test
     public void updateUser() {
         controller.createUser(user);
+
         assertEquals(controller.users.values().size(), 1);
         assertEquals(user.getId(), 1);
 
         user.setId(Integer.MIN_VALUE);
         ValidationException ex = assertThrows(ValidationException.class, () -> controller.updateUser(user));
+
         assertEquals(String.format("User with id: %s was not found!", user.getId()), ex.getMessage());
     }
 }
