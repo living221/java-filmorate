2d443cda42968d6ec906d8549a81c0629071d944
living221 <living221@gmail.com>
2023-02-28 00:34:45 +0700

    feat: added H2 database, schema and data.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,36 @@
+INSERT INTO GENRES (name) VALUES ( 'Action' );
+INSERT INTO GENRES (name) VALUES ( 'Adventure' );
+INSERT INTO GENRES (name) VALUES ( 'Animation' );
+INSERT INTO GENRES (name) VALUES ( 'Biography' );
+INSERT INTO GENRES (name) VALUES ( 'Comedy' );
+INSERT INTO GENRES (name) VALUES ( 'Crime' );
+INSERT INTO GENRES (name) VALUES ( 'Drama' );
+INSERT INTO GENRES (name) VALUES ( 'Family' );
+INSERT INTO GENRES (name) VALUES ( 'Fantasy' );
+INSERT INTO GENRES (name) VALUES ( 'Film-Noir' );
+INSERT INTO GENRES (name) VALUES ( 'History' );
+INSERT INTO GENRES (name) VALUES ( 'Horror' );
+INSERT INTO GENRES (name) VALUES ( 'Music' );
+INSERT INTO GENRES (name) VALUES ( 'Musical' );
+INSERT INTO GENRES (name) VALUES ( 'Mystery' );
+INSERT INTO GENRES (name) VALUES ( 'Romance' );
+INSERT INTO GENRES (name) VALUES ( 'Sci-Fi' );
+INSERT INTO GENRES (name) VALUES ( 'Sport' );
+INSERT INTO GENRES (name) VALUES ( 'Thriller' );
+INSERT INTO GENRES (name) VALUES ( 'War' );
+INSERT INTO GENRES (name) VALUES ( 'Western' );
+
+INSERT INTO ratings (name) VALUES('G');
+INSERT INTO ratings (name) VALUES('PG');
+INSERT INTO ratings (name) VALUES('PG-13');
+INSERT INTO ratings (name) VALUES('R');
+INSERT INTO ratings (name) VALUES('NC-17');
+
+
+
+
+
+
+
+
+
